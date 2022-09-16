28eac09eaa1b7b422ab66c89656a7c7a2530be2c
Pawar <pawar1550@gmail.com>
2022-09-16 11:41:26 -0400

    Add files via upload

--- Sep-14.R
+++ Sep-14.R
@@ -12,4 +12,9 @@ library(ggplot2)
 # Plots the time vs poison data
 ggplot(df, aes(x = poison, y = time, fill = poison)) + geom_boxplot() + geom_jitter(shape = 15, color = "steelblue", position = position_jitter(0.21)) + theme_classic()
 
+# Apply the function for calculating ANOVA test
+anova_one_way <- aov(time~poison, data = df); 
+summary(anova_one_way) 
+
+
 
